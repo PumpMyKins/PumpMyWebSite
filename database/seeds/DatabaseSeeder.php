cb0173fe2d416b1b11d1dbe3774c45325ad35641
Sufike <l.christ54260@gmail.com>
2019-02-16 22:11:59 +0100

    Disable test seeder only role seeder active

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(\CandidatureSeeder::class);
-        $this->call(\ServerSeeder::class);
+        // $this->call(\CandidatureSeeder::class);
+        // $this->call(\ServerSeeder::class);
         $this->call(\RolesSeeder::class);
     }
 }
