40cd9513e03c9ad9a2728882637d842a1c5c9e4a
clouis <clouis@debChr.cldom>
2020-08-23 00:11:42 +0200

    Remove local variable from session loading file

--- config/session.php
+++ config/session.php
@@ -155,7 +155,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', "newpmk.local"),
+    'domain' => env('SESSION_DOMAIN', null),
 
     /*
     |--------------------------------------------------------------------------
